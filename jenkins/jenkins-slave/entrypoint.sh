140120e50676fd586bb83a71c3b5f5abbf2cfe40
Yaroslav Domb <yaroslav.domb@gmail.com>
2026-04-29 16:17:21 +0300

    Fix project due to IPv6 issues

--- jenkins/jenkins-slave/entrypoint.sh
+++ jenkins/jenkins-slave/entrypoint.sh
@@ -5,12 +5,16 @@ set -e
 sudo dockerd \
     --host=unix:///var/run/docker.sock \
     --storage-driver=vfs \
+    --ipv6=false \
     &
  
 # Wait for Docker daemon to be ready
 echo "Waiting for Docker daemon..."
 timeout 30 sh -c 'until docker info > /dev/null 2>&1; do sleep 1; done'
 echo "Docker daemon is up."
+
+//My own issue - docker not works with IPv6, so I enforce it to use IPv4
+export JAVA_OPTS="-Djava.net.preferIPv4Stack=true"
  
 # Hand off to the official Jenkins inbound-agent entrypoint
 exec /usr/local/bin/jenkins-agent "$@"
